6fc07f085417191c9e80e0d173c5e0a08c090fa1
SimplePay <simplepaypro@users.noreply.github.com>
2015-05-24 22:18:03 +0300

    Initial version
    Initial version

--- result.php
+++ result.php
@@ -1,4 +1,9 @@
 <?
+//
+// Это обработчик Result для Вашего платежа
+// Перепишите методы process_success() и process_fail() в SimplePay.class.php
+//
+
 require_once("SimplePay.class.php");
 
 $simplepay = new SimplePay;

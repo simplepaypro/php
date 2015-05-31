d0b4f21bf88c2f9a7b842d5a2abdc864a542a106
SimplePay <simplepaypro@users.noreply.github.com>
2015-05-31 03:39:30 +0300

    Added comments

--- result.php
+++ result.php
@@ -1,4 +1,9 @@
 <?
+/* 
+SimplePay API PHP example (for PHP 5)
+https://simplepay.pro/
+*/
+
 //
 // Это обработчик Result для Вашего платежа
 // Перепишите методы process_success() и process_fail() в SimplePay.class.php
